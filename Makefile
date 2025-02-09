95def8096a94764a5ed67430eb594aee59424d19
ZHOU Cheng <c.zhou@live.com>
2025-02-09 20:42:15 +0800

    feat: add list command (#1)

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
-debug:
-	pdm build
-	pipx install --force dist/pifr-0.0.0-py3-none-any.whl
-	pifr --name Wink
+fmt:
+	pdm run ruff format src
+	pdm run ruff check --fix src
 
 publish:
 	pdm publish --verbose
+
+debug: fmt
+	pdm build
+	pipx uninstall pifr
+	pipx install --force dist/pifr-0.0.0-py3-none-any.whl
+	pifr --help
+	pifr list
--- src/pifr/main.py
+++ src/pifr/main.py
@@ -1,10 +1,21 @@
 import click
 
-@click.command()
-@click.option('--name', default='World', help='The name to greet.')
-def main(name):
-    """Simple command that greets NAME for a total of COUNT times."""
-    click.echo(f'Hello, {name}!')
+from pifr.commands.host import get_ssh_host_info, rich_print_hosts
 
-if __name__ == '__main__':
-    main()
+
+@click.group()
+def cli():
+    """Pull docker image from remote host."""
+
+
+@cli.command(name="list")
+def list_hosts():
+    """列出主机 list hosts"""
+    if hosts := get_ssh_host_info():
+        rich_print_hosts(hosts)
+    else:
+        click.echo(click.style("No hosts found in ~/.ssh/config", fg="red"))
+
+
+if __name__ == "__main__":
+    cli()
